8abf116674c745c1e37c9fb04b6adb7556fe3762
Maaz Fitter <maazfitter@gmail.com>
2020-12-18 14:13:28 -0600

    Commented out ticket creation

--- src/main/java/com/vtheatre/service/StripeServiceImpl.java
+++ src/main/java/com/vtheatre/service/StripeServiceImpl.java
@@ -8,9 +8,9 @@ import com.stripe.model.Charge;
 import com.stripe.param.ChargeCreateParams;
 import com.vtheatre.data.entity.Ticket;
 import com.vtheatre.data.model.PaymentRequest;
-import com.vtheatre.util.TicketUtils;
+// import com.vtheatre.util.TicketUtils;
 
-import org.springframework.beans.factory.annotation.Autowired;
+// import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Component;
 
@@ -20,8 +20,8 @@ public class StripeServiceImpl implements StripeService {
     @Value("${stripe.apiKey}")
     private String apiKey;
 
-    @Autowired
-    private TicketService ticketService;
+    // @Autowired
+    // private TicketService ticketService;
 
     @PostConstruct
     public void init() {
@@ -31,19 +31,19 @@ public class StripeServiceImpl implements StripeService {
     @Override
     public Ticket Charge(PaymentRequest paymentRequest) {
         Ticket ticket = null;
-        Charge charge = null;
+        // Charge charge = null;
 
         ChargeCreateParams params = ChargeCreateParams.builder().setAmount(paymentRequest.getAmount() * 100L)
                 .setCurrency(paymentRequest.getCurrency()).setDescription(paymentRequest.getDescription())
                 .setSource(paymentRequest.getTokenId()).setReceiptEmail(paymentRequest.getEmailAddress()).build();
 
         try {
-            charge = Charge.create(params);
+            Charge.create(params);
             // generate a ticket confirmation code
-            if (charge.getCaptured()) {
-                String confirmationCode = TicketUtils.confirmationCodeGenerator();
-                ticket = ticketService.createTicket(confirmationCode, charge, paymentRequest.getShowtime());
-            }
+            // if (charge.getCaptured()) {
+            //     String confirmationCode = TicketUtils.confirmationCodeGenerator();
+            //     ticket = ticketService.createTicket(confirmationCode, charge, paymentRequest.getShowtime());
+            // }
         } catch (StripeException e) {
             e.printStackTrace();
         }
